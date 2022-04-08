bbbe10cdac6d9b350df64d79750d4176520f6c4e
Rakunanthan Subramanian <rakunanthan@Rakunanthans-MacBook-Pro.local>
2022-04-08 15:33:06 +0100

    updated user login dashboard

---
+++ client/src/components/dashboard/Userdashboard.js
@@ -0,0 +1,55 @@
+
+import React ,{Fragment, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+ import PropTypes from 'prop-types'
+ import { connect } from 'react-redux'
+ import Spinner from '../Layout/Spinner'
+ import {getCurrentProfile } from '../../actions/profile'
+ import DashboardActions from './DashboardActions'
+
+ 
+ const Userdashboard = ({getCurrentProfile,auth:{user},profile:{profile,loading}}) => {
+   useEffect(()=>{
+     getCurrentProfile();
+   },[]);
+   return loading && profile ===null ? <Spinner/> :
+   <div style={{
+     "margin-top":"100px"
+
+   }}>
+   
+   
+   <Fragment>
+     <h1 className='large text-primary'> User Dashboard </h1>
+     <p className='lead'>
+       <i className='fas fa-user'></i> welcome {user && user.name}
+     </p>
+    {profile !==null ? (<Fragment>
+      <DashboardActions/>
+    </Fragment>) : (<Fragment>
+     <p>Still You have not created any parking space</p>
+     
+     <Link to="/Createprofile" className="btn btn-primary my-1">
+            Register your Parking Space
+          </Link>
+   </Fragment>)
+ }
+ </Fragment>
+ </div>
+ }
+
+ 
+ Userdashboard.propTypes = {
+   getCurrentProfile: PropTypes.func.isRequired,
+  auth:PropTypes.object.isRequired,
+  profile:PropTypes.object.isRequired
+ }
+
+ const mapStateToProps =state=>
+ ({
+   auth:state.auth,
+   profile:state.profile
+ });
+ 
+ export default connect(mapStateToProps,{getCurrentProfile})(Userdashboard);  
+
